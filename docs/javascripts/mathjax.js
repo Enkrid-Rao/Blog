105cf2477a3373f1a75c876ea6832ed2656f7fbf
Enkrid-Rao <xinxingrao@outlook.com>
2026-05-21 23:25:42 +0800

    fix: 修复标题和侧边栏公式不渲染

--- docs/javascripts/mathjax.js
+++ docs/javascripts/mathjax.js
@@ -8,4 +8,16 @@ window.MathJax = {
     ignoreHtmlClass: ".*|",
     processHtmlClass: "arithmatex",
   },
+  startup: {
+    ready: function () {
+      MathJax.startup.defaultReady();
+      // 渲染侧边栏和标题中的公式
+      MathJax.startup.promise.then(function () {
+        var navItems = document.querySelectorAll(".md-nav__link, .md-nav__title, h1, h2, h3");
+        navItems.forEach(function (item) {
+          MathJax.typesetPromise([item]);
+        });
+      });
+    },
+  },
 };
